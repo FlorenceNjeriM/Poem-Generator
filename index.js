b7668f64294986672570fab1b3304938ba869fe4
FlorenceNjeriM <159240271+FlorenceNjeriM@users.noreply.github.com>
2025-02-24 12:39:46 +0300

    Added a basic form handling and added typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Hello world");
+function generatePoem (event){
+    event.preventDefault();
+
+    new Typewriter('#poem', {
+        strings: "I must go down to the seas again, to the lonely sea and the sky",
+        autoStart: true,
+        display: 1,
+        cursor: "",
+      });
+    
+}
+
+
+let poemFormElement= document.querySelector("#generator-poem-form")
+poemFormElement.addEventListener("submit", generatePoem)
+
